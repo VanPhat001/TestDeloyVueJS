ee47d711703cd581be5de872aced42cfe2c95cd7
VanPhat001 <vanphatct2002@gmail.com>
2024-03-23 01:26:06 +0700

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,98 @@
 </template>
 
 
-<script setup>
+<script setup>// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+
+
+const provider = new GoogleAuthProvider();
+
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD41-TjARiscGfh1NtP6QQig0Zbv6HPCvY",
+    authDomain: "unityfirebase-c1a95.firebaseapp.com",
+    projectId: "unityfirebase-c1a95",
+    storageBucket: "unityfirebase-c1a95.appspot.com",
+    messagingSenderId: "833216891654",
+    appId: "1:833216891654:web:6ccff82c0c620487d9ac87",
+    measurementId: "G-P3VMGW43Z6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const email = "vanphatct2002@gmail.com"
+const password = "VanPhat2002"
+
+
+// createUserWithEmailAndPassword(auth, email, password)
+//   .then((userCredential) => {
+//     // Signed up 
+//     const user = userCredential.user;
+//     // ...
+//   })
+//   .catch((error) => {
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // ..
+//   });
+
+
+
+// signInWithEmailAndPassword(auth, email, password)
+//     .then((userCredential) => {
+//         // Signed in 
+//         const user = userCredential.user;
+//         // ...
+//         console.log(user);
+//     })
+//     .catch((error) => {
+//         const errorCode = error.code;
+//         const errorMessage = error.message;
+//         console.log(errorMessage);
+//     });
+
+
+signInWithPopup(auth, provider)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.log({
+            token,
+            user
+        });
+
+    }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+
+        console.log({
+            errorCode,
+            errorMessage,
+            email,
+            credential
+        });
+    });
 
 </script>
